8e46b8c65da7c1ecf96e2eb4f9d650c6f98fa73e
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-04 09:49:33 -0300

    Implemento auction y user para que pasen los tests

--- src/model/Auction.js
+++ src/model/Auction.js
@@ -1,6 +1,39 @@
+import {
+    AuctionStateBuilder
+} from './Builders';
+import {
+    User
+} from './User';
+
 export class Auction {
-    canUserBet(user){
-        return false;
+
+    constructor(state) {
+        this.owner = User.null()
+        this.lastBettor = User.null()
+        this.state = state || new AuctionStateBuilder().new().build();
+    }
+
+    start() {
+        this.state = this.state.start();
+    }
+
+    isInProgress() {
+        return this.state.isInProgress();
+    }
+
+    canUserBet(user) {
+        return this.isInProgress() &&
+            !user.isAnonymous() &&
+            !this.isLastBettor(user) &&
+            !this.isOwner(user);
+    }
+
+    isLastBettor(user) {
+        return this.lastBettor.equals(user);
+    }
+
+    isOwner(user) {
+        return this.owner.equals(user);
     }
 }
 
--- src/model/User.js
+++ src/model/User.js
@@ -1,5 +1,15 @@
 export class User {
-    
+    static null(){
+        return {equals: (user) => false}
+    }
+
+    isAnonymous(){
+        return this.anonymous;
+    }
+
+    equals(user) {
+        return this == user
+    }
 }
 
 export default User;
