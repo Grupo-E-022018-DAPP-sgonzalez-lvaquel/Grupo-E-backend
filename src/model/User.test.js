699bbcf3634969ca38b9d86df05f8422d40517cd
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-04 17:53:39 -0300

    Agrego tests de usuario

--- src/model/User.test.js
+++ src/model/User.test.js
@@ -1,16 +1,46 @@
+import { UserBuilder } from './Builders';
+
 describe('User', () => {
     describe('auction behaviour', () => {
-        xtest('a user can create an auction', () => {
+        let owner;
+
+        beforeEach(() => {
+            owner = new UserBuilder().build();
+        });
+
+        test('a user can create an auction', () => {
+            // Exercise
+            expect(owner.newAuction()).toBeTruthy();
         });
 
         describe('auction betting', () => {
-            xtest('a user can not bet in his own auction', () => {
+            let auction;
+            let anyOtherUser;
+
+            beforeEach(() => {
+                auction = owner.newAuction();
+                anyOtherUser = new UserBuilder().build();
             });
-            xtest('a user can not bet in an auction if it is new', () => {
+
+            test('a user can not bet in his own auction', () => {
+                // Exercise
+                expect(auction.canUserBet(owner)).toBeFalsy();
             });
-            xtest('a user can bet in an auction if it is in progress', () => {
+            test('a user can not bet in an auction if it is new', () => {
+                // Exercise
+                expect(auction.canUserBet(anyOtherUser)).toBeFalsy();
             });
-            xtest('a user can not bet in an auction if it is finished', () => {
+            test('a user can bet in an auction if it is in progress', () => {
+                // Exercise
+                auction.start();
+
+                expect(auction.canUserBet(anyOtherUser)).toBeTruthy();
+            });
+            test('a user can not bet in an auction if it is finished', () => {
+                // Exercise
+                auction.finish();
+
+                expect(auction.canUserBet(anyOtherUser)).toBeTruthy();
             });
         });
     });
