33197b41fb8fef0fc9488a8fc233fb5aa03fb6c3
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-09-04 17:39:24 -0300

    Skipeo tests de user

--- src/model/User.test.js
+++ src/model/User.test.js
@@ -1,21 +1,16 @@
 describe('User', () => {
     describe('auction behaviour', () => {
-        test('a user can create an auction', () => {
-            expect(true);
+        xtest('a user can create an auction', () => {
         });
 
         describe('auction betting', () => {
-            test('a user can not bet in his own auction', () => {
-                expect(true);
+            xtest('a user can not bet in his own auction', () => {
             });
-            test('a user can not bet in an auction if it is new', () => {
-                expect(true);
+            xtest('a user can not bet in an auction if it is new', () => {
             });
-            test('a user can bet in an auction if it is in progress', () => {
-                expect(true);
+            xtest('a user can bet in an auction if it is in progress', () => {
             });
-            test('a user can not bet in an auction if it is finished', () => {
-                expect(true);
+            xtest('a user can not bet in an auction if it is finished', () => {
             });
         });
     });
