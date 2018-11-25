e0896396c838baf599c700f87298264fa77dd81a
Sebastian Gonzalez <sebastian.h.gonzalez@gmail.com>
2018-11-25 11:57:07 -0300

    Added kId to user schema

--- src/Schemas/UserSchema/UserSchema.js
+++ src/Schemas/UserSchema/UserSchema.js
@@ -1,7 +1,11 @@
 export function UserSchema({
+    Sequelize,
     sequelize,
 }) {
     return sequelize.define('user', {
+        kid: {
+            type: Sequelize.STRING,
+        }
     }, {
         paranoid: true,
     });
